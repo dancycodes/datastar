6e2883b58e9adf556a2117452b760c8b8918f9bc
Ben Croker <ben@putyourlightson.com>
2025-07-25 17:52:15 +0200

    Fix

--- resources/views/components/tasks/item.blade.php
+++ resources/views/components/tasks/item.blade.php
@@ -26,7 +26,7 @@
             </button>
             <button
                 title="delete"
-                data-on-click="get-form{{ datastar()->delete(['TaskController', 'destroy'], ['task' => $task->id]) }}"
+                data-on-click="{{ datastar()->delete(['TaskController', 'destroy'], ['task' => $task->id]) }}"
             >
                 <svg class="w-5 h-5 text-red-800 cursor-pointer hover:text-red-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z"/>
