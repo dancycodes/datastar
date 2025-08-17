ab3034e7ce738f2ff05a315165b809cb7a7b1dc1
gouater <dancycodes@gmail.com>
2025-08-17 21:51:05 +0100

    Add dump and dd example route for tasks
    Introduces a new '/dump' route handled by TaskController@dump, which demonstrates usage of dump and dd. A link to this route is added to the todos page for testing and debugging purposes.

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/dump', [TaskController::class, 'dump'])
+    ->name('tasks.dump')
+    ->middleware('verified');
+
 Route::get('/tasks/get-form/{task}', [TaskController::class, 'getForm'])
     ->name('tasks.getForm')
     ->middleware('verified');
